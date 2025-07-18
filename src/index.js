42b64233810a60149c6e5c177dc0af0a6996caeb
Lalit Pandey <pandeylalit147@gmail.com>
2025-07-18 22:33:01 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// require('dotenv').config({path:'./env'}); // Load environment variables from .env file
+import dotenv from 'dotenv';
+import connectDB from './db/db.js';
+
+dotenv.config({ 
+   path: './env'
+ }); // Load environment variables from .env file
+
+connectDB()
