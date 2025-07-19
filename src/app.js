fc44a4a628224e653630ff03b99101056cf4fe87
Lalit Pandey <pandeylalit147@gmail.com>
2025-07-19 21:37:25 +0530

    do changes

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const app = express();
+import cors from 'cors'; 
+import cookieParser from 'cookie-parser';
+
+app.use(cors({
+   origin: process.env.CORS_ORIGIN,
+   credentials: true // Allow credentials (cookies, authorization headers, etc.)
+}));
+
+app.use(express.json({limit: '2mb'})); // Limit request body size to 2MB
+app.use(express.urlencoded({extended: true, limit: '2mb'})); // Limit URL-encoded data size to 2MB
+app.use(express.static('public')); // Serve static files from the 'public' directory
+app.use(cookieParser())
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import dotenv from 'dotenv';
 import connectDB from './db/db.js';
 
+
 dotenv.config({ 
    path: './env'
  }); // Load environment variables from .env file
