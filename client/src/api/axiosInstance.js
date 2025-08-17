cf1c38e8004ffb026cc6b96d6e68955a6b04af26
Lalit Pandey <pandeylalit147@gmail.com>
2025-08-17 13:26:37 +0530

    check

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:8000/api/v1", // change this to your backend base URL
-  withCredentials: true, // if you’re using cookies for auth
+  withCredentials: true,
 });
 
 export default axiosInstance;
