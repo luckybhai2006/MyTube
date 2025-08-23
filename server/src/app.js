4fc20f8fb2d3d0fc4aed0020fe0485d764da50a7
Lalit Pandey <pandeylalit147@gmail.com>
2025-08-23 18:44:38 +0530

    ,,,

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    credentials: false, // Allow credentials (cookies, authorization headers, etc.)
   })
 );
 
